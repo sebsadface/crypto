f67549a4aa4d72c0f7dff240ae7598f879605942
Sebastian Liu <sebsadface@gmail.com>
2023-11-12 00:29:04 -0800

    timing side channel attack

--- hw4/hw4-etm.py
+++ hw4/hw4-etm.py
@@ -60,8 +60,80 @@ cipher = EtMEncrypt(key_enc, key_mac, b"Hello world!")
 # =======================
 # Insert your attack below this line.
 # Do not modify above this line.
+def measure_baseline_time(oracle, cipher, byte_index, trials=100):
+    total_time = 0
+    test_cipher = cipher
+    if byte_index > 0:
+        wrong_tag = b'\x00' * byte_index  
+        test_cipher = cipher[:-byte_index] + wrong_tag
 
-cipher_forged = cipher
+    for _ in range(trials):
+        start = timer()
+        oracle(test_cipher)
+        end = timer()
+        total_time += (end - start)
+
+    return total_time / trials
+
+
+def modify_cipher_text(cipher, original="Hello world!", new="Hello folks!"):
+    # XOR the original and new text to get the XOR difference
+    xor_diff = bytes([a ^ b for a, b in zip(original.encode(), new.encode())])
+
+    # Apply this XOR difference to the corresponding part of the cipher text
+    modified_cipher = bytearray(cipher)
+    start_index = 16 
+
+    for i, byte in enumerate(xor_diff):
+        modified_cipher[start_index + i] ^= byte
+
+    return bytes(modified_cipher)
+
+def guess_tag_byte(oracle, altered_cipher, guessed_tag, byte_index, baseline_time):
+    correct_guess = 0
+  
+    while correct_guess == 0:
+        for guess in range(256):
+            guessed_tag[byte_index] = guess
+            test_cipher = altered_cipher[:-8] + guessed_tag
+
+            start = timer()
+            oracle(test_cipher)
+            end = timer()
+            time = (end - start)
+
+            if time * 1.01 >= baseline_time :
+                total_time = 0
+                for _ in range (100):
+                    start = timer()
+                    oracle(test_cipher)
+                    end = timer()
+                    total_time += (end - start)
+                avg = total_time / 100
+
+                print(f"Byte {byte_index}, Guess {guess}, Time: {avg}") 
+                print()
+                if (avg * 1.01 >= baseline_time ):
+                    correct_guess = guess
+                    break
+                continue 
+    
+    return correct_guess
+
+beginning = timer()
+
+guessed_tag = bytearray(8)
+altered_cipher = modify_cipher_text(cipher, original="Hello world!", new="Hello folks!")
+
+for i in range(8):
+    baseline_time = measure_baseline_time(EtMOracle, cipher, 7 - i)
+    print(f"Baseline Time: {baseline_time}") 
+    guessed_tag[i] = guess_tag_byte(EtMOracle, altered_cipher, guessed_tag, i, baseline_time)
+    print(f"Guessed Tag So Far: {guessed_tag.hex()}") 
+cipher_forged = altered_cipher[:-8] + guessed_tag
+ending = timer()
+print()
+print(f"Total Time: {ending - beginning}")  
 
 # Insert the attack above this line.
 # Do not mofidy below this line.
